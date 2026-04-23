8398d86956f8d9a275fa095e24df43bb7d343e37
luckykingmendoza <luckykingmendoza03@gmail.com>
2026-04-23 16:28:04 +0800

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "918102514" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3124433504" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
